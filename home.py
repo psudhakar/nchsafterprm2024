e35a42d1c1e1101fab53b261beeabe16dcd534fd
psudhakar <sudhakar.parsi@gmail.com>
2024-04-04 14:01:24 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,9 +1,22 @@
 import streamlit as st
-def detect_browser():
-    if st.config.get_option("browser.gatherUsageStats"):
-        return True
-    else:
-        return  False
+import httpx
+
+# Get the user-agent string
+@st.cache_data()
+def get_user_agent():
+    try:
+        user_agent = httpx.get("https://httpbin.org/user-agent").json()["user-agent"]
+    except Exception as e:
+        user_agent = "Unknown"
+    return user_agent
+
+# Detect if the user is on a mobile device
+def is_mobile(user_agent):
+    mobile_keywords = ["Mobile", "iPhone", "iPad", "Android"]
+    for keyword in mobile_keywords:
+        if keyword in user_agent:
+            return True
+    return False
 
 # Setting the page configuration with a background image
 st.set_page_config(
@@ -18,12 +31,15 @@ def set_bg_image(img_binary):
         f.write(img_binary)
     st.beta_set_background_image("background.jpg")
 
-is_desktop = detect_browser()
+# Get the user-agent string
+user_agent = get_user_agent()
+
+is_mobile = is_mobile(user_agent)
 
 col1, col2 = st.columns([1,6])
 
 
-if is_desktop:
+if not is_mobile:
     with col1:
         st.image('nchslogo.jpg', width=100)  # Adjust width as needed
 
