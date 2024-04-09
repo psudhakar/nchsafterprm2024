c200fa9b06b0e9da25d0aec46f9ebad8ee5f925b
psudhakar <sudhakar.parsi@gmail.com>
2024-04-09 12:50:43 -0500

    Update home.py

--- home.py
+++ home.py
@@ -54,6 +54,8 @@ def set_bg_image(img_binary):
         f.write(img_binary)
     st.beta_set_background_image("background.jpg")
 
+
+
 col1, col2 = st.columns([1,6])
 
 with col1:
@@ -75,65 +77,100 @@ with col2:
             unsafe_allow_html=True,
         )
 
-sub_header = "Join us for an unforgettable night!"
 
-# Wrap the text in markdown with center alignment style
-centered_text = f"<h3 style='text-align: center;'>{sub_header}</h2>"
+#col1, col2 = st.columns(2)
 
-# Display the centered markdown
-st.markdown(centered_text, unsafe_allow_html=True)
+#with col1:
+#     st.markdown("")
+#with col2:
+#    st.markdown("")
+#    with st.container(border=True): 
+#        col1, col2, col3 = st.columns(3)
+#        with col1:
+#            st.markdown("[Normal Community High School](https://www.unit5.org/NCHS)")
+#        with col2:
+#            st.markdown("[Buy Tickets](https://gofan.co/app/school/IL21465)")
+#        with col3:
+#            st.markdown("[Directions](https://nchsafterprom.ticketspice.com/preview/abc58282cedf4ab2829ee2ff7ceca18d#directions)")
+
+  
+col1, col2 = st.columns(2)
+
+with col1:
+    event_details = """
+    ## Prom night ends, After Prom adventure begins !
+    Join us for an epic After Prom Party, an unforgettable journey beyond the ballroom.
 
 
-event_details = """
-The After Prom party is a safe and fun way to celebrate the end of prom night with your friends. 
-This year's theme is **Met Gala Vegas Style**. 
-Get ready for a night of glitz, glamour, and excitement!
 
-**Date:** Saturday, April 27th, 2024
-**Time:** 11:00 PM - 3:00 AM
-**Location:** Normal Community High School
-"""
-with st.container(border=True):
-      st.markdown(event_details)
+    :blue[**The Amazing Laser Tag**]: Strategize your squad and gear up for an unforgettable battle royale between multiple teams. An adrenaline pumping experience in a 50 ft by 50 ft maze
 
-event_details2 = """
-**Activities:**
+    :orange[**Interactive games**]: Challenge your friends to classic Inflatable competitive fun. Putt your way to victory with "Indoor Mini Golf" with 9 hole course. Work together in a thrilling "Light-Up Cones match" and much much more. Win amazing prizes for some of the interactive games.
 
-* DJ Lights, Music & Lasers
-* Amazing Laser Tag
-* Interactive Games
-* Food Fiesta...and much more!
+    :green[**Food Fiesta**]: Fuel up with an explosion of delicious finger foods, snacks, soft drinks & party treats to keep you hydrated & charged up throughout the night
 
-**Prizes:**
+    :red[**The Main Event**]: Electrify the night with our Celebrity guests from Chicago, the Z-LED Bots, all under the dazzling display of DJ lights, pulsing sounds & heart-pumping music
 
-* Grand Prize: Macbook 15
-* Over $4000 in giveaways!
+    :blue[**Grand Finale**]: Get ready for college by gearing up for an epic giveaway, with the chance to win over $6000 in prizes. Create memories that will last long after the music & Afterprom fades away!
+    """
+    with st.container(border=True):
+        st.markdown(event_details, unsafe_allow_html=True )
 
-"""
+with col2:
+    event_details3 = """
+    #### :blue[Upcoming Schedule]: Special Promo - 12 Days of Afterprom:
 
-event_details3 = """
-**:blue[Upcoming Schedule]: Special Promo - 12 Days of Afterprom:**
+    - Buy Afterprom tickets early, and increase your chances to win a $15 gift card, each day for 12-days.
+    - Winners will be announced daily on AfterProm Instagram page & at school!
 
-- Buy Afterprom tickets early, and increase your chances to win a $15 gift card, each day for 12-days.
-- Winners will be announced daily on AfterProm Instagram page & at school!
+    **Tickets:** $14 ( Tickets can be purchased through the school's [GoFan](https://gofan.co/app/school/IL21465) page )
+    """
+     
+    with st.container(border=True):
+        st.markdown(event_details3, unsafe_allow_html=True )
 
-**Tickets:** $14 ( Tickets can be purchased through the school's [GoFan](https://gofan.co/app/school/IL21465) page )
-"""
+        col1, col2 = st.columns([1,4])
+       
+        with col1:
+             st.markdown("**Location:**")
+        with col2: 
+            st.markdown("3900 E Raab Rd, Normal, IL 61761, USA, Normal, IL, 61761 US")
 
-col1, col2 = st.columns([1,3])
+        with col1:
+             st.markdown("**School web site:**")
+        with col2: 
+            st.markdown("[Normal Community High School](https://www.unit5.org/NCHS)")
 
-with st.container(border=True):
-    with col1:
-        st.markdown(event_details2, unsafe_allow_html=True)
-    with col2:
-        st.markdown(event_details3, unsafe_allow_html=True)
+        with col1:
+             st.markdown("**Buy Tickets:**")
+        with col2:
+            st.markdown("[School GoFan site](https://gofan.co/app/school/IL21465) (Only **$14** for AfterProm tickets from GoFan. Use Access Code: :green[PROM24])")
 
+        with col1:       
+            st.markdown("**Directions:**")
+        with col2:
+             st.markdown("[Google](https://maps.google.com/?daddr=3900%20E%20Raab%20Rd%2C%20Normal%2C%20IL%2061761%2C%20USA%2C%20Normal%20IL%2061761%20US) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [Bing](https://www.bing.com/maps/default.aspx?rtp=~adr.3900%20E%20Raab%20Rd%2C%20Normal%2C%20IL%2061761%2C%20USA%2C%20Normal%20IL%2061761%20US) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [MapQuest](http://mapq.st/directions?saddr=&daddr=3900%20E%20Raab%20Rd%2C%20Normal%2C%20IL%2061761%2C%20USA%2C%20Normal%20IL%2061761%20US)")
 
 st.image(
     "NCHSAfterPromFlyer.jpg",  # Replace with appropriate image
-    width=700,
-    caption="Vegas Style After Prom!",
+    use_column_width=True
 )
+
+#sub_header = "Join us for an unforgettable night!"
+# Wrap the text in markdown with center alignment style
+#centered_text = f"<h3 style='text-align: center;'>{sub_header}</h2>"
+# Display the centered markdown
+#st.markdown(centered_text, unsafe_allow_html=True)
+
+        
+
+
+
+#st.image(
+#    "NCHSAfterPromFlyer.jpg",  # Replace with appropriate image
+#    width=700,
+#    caption="Vegas Style After Prom!",
+#)
 
 # Contact information
 contact_info = """
